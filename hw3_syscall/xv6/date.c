d77af3cc0abb31125f611d9529949feca1753e8f
playdougher <952331408@qq.com>
2020-05-13 14:07:29 +0800

    update

---
+++ hw3_syscall/xv6/date.c
@@ -0,0 +1,18 @@
+#include "types.h"
+#include "user.h"
+#include "date.h"
+
+int
+main(int argc, char *argv[])
+{
+  struct rtcdate r;
+
+  if (date(&r)) {
+    printf(2, "date failed\n");
+    exit();
+  }
+
+  // your code to print the time in any format you like...
+  printf(1, "month/day/year time : %d/%d/%d %dh:%dm:%ds\n", r.month, r.day, r.year,r.hour, r.minute, r.second);
+  exit();
+}
